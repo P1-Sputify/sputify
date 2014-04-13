a4776a22d0e6cc47d5b8575c0483f79f30cad41b
Nethakaaru <sebastian.aspegren@telia.com>
2014-04-13 20:09:01 +0200

    Proof that sound can be played

--- P1-Sputify/src/Server/KlientTest.java
+++ P1-Sputify/src/Server/KlientTest.java
@@ -9,14 +9,25 @@ import java.io.*;
 public class KlientTest {
 	private String serverIP;
 	private int serverPort;
+	
 
 	public KlientTest(String serverIP, int serverPort) {
+		playSound();
 		this.serverIP = serverIP;
 		this.serverPort = serverPort;
 		Thread thread = new Thread(new ConnectAndListenToServer());
 		thread.start();
 	}
-
+	 public void playSound() {
+	     try {
+	         AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("wavfiles/finishhim.wav").getAbsoluteFile());
+	         Clip clip = AudioSystem.getClip();
+	         clip.open(audioInputStream);
+	         clip.start();
+	     } catch(Exception ex) {
+	         System.out.println("Error with playing sound.");
+	         ex.printStackTrace();
+	     }}
 	private class ConnectAndListenToServer implements Runnable {
 		public void run() {
 			try {
@@ -28,23 +39,24 @@ public class KlientTest {
 			byte[] sound;
 				while (true) {
 					
-					try{
-						sound=(byte[]) input.readObject();
-						System.out.println("read file ");
-						InputStream myInputStream = new ByteArrayInputStream(sound); 
-					AudioInputStream audioInputStream=AudioSystem.getAudioInputStream(myInputStream);
-					System.out.println("get input stream");
-				Clip clip=AudioSystem.getClip();
-				clip.open();
-			clip.start();
-			System.out.println("Play clip");
+					    try {
+					    	sound=(byte[] ) input.readObject();
+					    	InputStream myInputStream = new ByteArrayInputStream(sound);
+					        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(myInputStream);
+					        Clip clip = AudioSystem.getClip();
+					        clip.open(audioInputStream);
+					        clip.start();
+					    } catch(Exception ex) {
+					        System.out.println("Error with playing sound.");
+					        ex.printStackTrace();
+					    
 					}
-					catch(Exception ex){}
 				}
 			} catch (IOException e) {
 				System.out.println(e);
+					
 			} 
-		}
+	}
 	}
 
 	public static void main(String[] args) {
