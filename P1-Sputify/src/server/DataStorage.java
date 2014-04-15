180483101d8cf2bdd2fec2f1c1722adf15cf824d
Nethakaaru <sebastian.aspegren@telia.com>
2014-04-15 15:51:18 +0200

    Added methods to read the textfiles with users and tracks and load them into hash tables.

--- P1-Sputify/src/server/DataStorage.java
+++ P1-Sputify/src/server/DataStorage.java
@@ -3,14 +3,26 @@
  */
 package server;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Hashtable;
+
 /**
  * @author Nethakaaru
  * 
  */
 public class DataStorage {
 	//TrackID as key.
-	private Hashtable<String, Track> tracks;
+	private Hashtable<String, Track> trackList= new Hashtable<String,Track>();
+	//UserID as key
+	private Hashtable<String, User> userList= new Hashtable<String,User>();
+	
+	public DataStorage(){
+		loadTracks("textFiles/Tracks.txt");
+		loadUsers("textFiles/Users.txt");
+	}
 
 	/**
 	 * A method used to get a track with a specific id.
@@ -19,9 +31,9 @@ public class DataStorage {
 	 *            The id of the track we want to find.
 	 * @return The track if it was found, if not return null.
 	 */
-	public Track getTrack(int trackId) {
+	public Track getTrack(String trackId) {
 
-		return null;
+		return trackList.get(trackId);
 
 	}
 
@@ -38,5 +50,64 @@ public class DataStorage {
 		return null;
 
 	}
+	private void loadTracks(String fileName)
+	{
+		try
+		{
+			FileInputStream fis = new FileInputStream(fileName);
+			InputStreamReader ir = new InputStreamReader(fis);
+			BufferedReader br = new BufferedReader(ir);
 
+			String[] parts;
+			String txt = br.readLine();
+
+			while (txt != null)
+			{
+				parts = txt.split(";");
+
+				User user = new User(Integer.parseInt(parts[0]), parts[1], parts[2], parts[3]);
+				userList.put(parts[0], user);
+				txt = br.readLine();
+			}
+		} catch (IOException e)
+		{
+			System.err.println("IOException: " + e.getMessage());
+
+		}
+		{
+
+		}
+	}
+	private void loadUsers(String fileName)
+	{
+		try
+		{
+			FileInputStream fis = new FileInputStream(fileName);
+			InputStreamReader ir = new InputStreamReader(fis);
+			BufferedReader br = new BufferedReader(ir);
+
+			String[] parts;
+			String txt = br.readLine();
+
+			while (txt != null)
+			{
+				parts = txt.split(";");
+
+				Track track = new Track(parts[0], parts[1], parts[2], parts[3],Integer.parseInt(parts[4]), parts[5]);
+				trackList.put(parts[0], track);
+				txt = br.readLine();
+			}
+		} catch (IOException e)
+		{
+			System.err.println("IOException: " + e.getMessage());
+
+		}
+		{
+
+		}
+	}
+	public static void main(String[]args){
+		DataStorage ds= new DataStorage();
+		System.out.println(ds.getTrack("7").toString());
+	}
 }
