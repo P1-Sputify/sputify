db3819fb457dd9eeef9c526503606852ad11738c
Sailor46 <amir.mehmedagic@gmail.com>
2014-05-07 12:11:13 +0200

    Updates i Controller and KlientTest

--- P1-Sputify/src/Server/Controller.java
+++ P1-Sputify/src/Server/Controller.java
@@ -6,6 +6,10 @@ package Server;
 import java.net.*;
 import java.io.*;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 /**
  * @authors mehmedagica, Sebastian Aspegren
  *
@@ -58,7 +62,7 @@ public class Controller {
 				System.out.println("Server socket created on port " + port);
 				while( true ) {
 					socket = serverSocket.accept(); // Här ska kommunikationen med klienten startas
-					System.out.println("Server socket accept client");
+					System.out.println("Server socket accept client connection");
 					clientThread = new Thread(new TalkToClient(socket));
 					clientThread.start();
 					System.out.println("A new client thread created and started");
@@ -99,12 +103,8 @@ public class Controller {
 				//Get user name and password
 				//as string array
 				String[] userLogin = null;
-				Boolean waitForUserLogin = true;
-				while(waitForUserLogin) {
 					userLogin = (String[])ois.readObject();
-					System.out.println("User login received");
-					waitForUserLogin = false;
-				}
+					System.out.println("User login received: " + userLogin[0] + " " + userLogin[1]);
 				
 				//If user is OK then send track list to the client
 				if(DataStorage.verifyUser(userLogin[0], userLogin[1])) {
@@ -124,13 +124,66 @@ public class Controller {
 					waitForTrackId = false;
 				}
 				
+				
+				
 				//Send audio file to the client
 				if (trackId > 0) {
-					//Here comes code to send the audio file to the client
-					oos.writeObject(DataStorage.loadAudioFile(trackId));
-					oos.flush();
-					System.out.println("File sent to the client");
-					Thread.sleep( 3000 );
+//					//Here comes code to send the audio file to the client
+//					oos.writeObject(DataStorage.loadAudioFile(trackId));
+//					oos.flush();
+//					System.out.println("File sent to the client");
+//					Thread.sleep( 3000 );
+					
+					int totalFramesRead = 0;
+					//File fileIn = new File(DataStorage.getTrack(trackId).getLocation());
+					//C:/WavFiles/tones_100-900_8_bit_square.wav
+					File fileIn = new File("wavfiles/finishhim.wav");
+					byte[] audioBytes = null;
+					FileOutputStream fos = null;
+					BufferedOutputStream bos = null;
+					// somePathName is a pre-existing string whose value was
+					// based on a user selection.
+					try {
+					  AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(fileIn);
+					  int bytesPerFrame = audioInputStream.getFormat().getFrameSize();
+					    if (bytesPerFrame == AudioSystem.NOT_SPECIFIED) {
+					    // some audio formats may have unspecified frame size
+					    // in that case we may read any amount of bytes
+					    bytesPerFrame = 1;
+					  } 
+					  // Set an arbitrary buffer size of 1024 frames.
+					  int numBytes = 1024 * bytesPerFrame; 
+					  audioBytes = new byte[numBytes];
+					  try {
+					    int numBytesRead = 0;
+					    int numFramesRead = 0;
+					    
+					    fos = new FileOutputStream(fileIn);
+						bos = new BufferedOutputStream(fos);
+					    
+					    // Try to read numBytes bytes from the file.
+					    while ((numBytesRead = audioInputStream.read(audioBytes)) > 0) {
+					      // Calculate the number of frames actually read.
+					      numFramesRead = numBytesRead / bytesPerFrame;
+					      totalFramesRead += numFramesRead;
+					      // Here, do something useful with the audio data that's 
+					      // now in the audioBytes array...
+					      bos.write(audioBytes, 0, numBytesRead);
+					    }
+					    
+					    bos.flush();
+						bos.close();
+						audioInputStream.close();
+						System.out.println("File sent to the client");
+						Thread.sleep( 3000 );
+					  
+					  } catch (IOException e1) {
+							System.out.println(e1);
+					  }
+						
+					} catch (IOException | UnsupportedAudioFileException e) {
+						System.out.println(e);
+					}
 				}
 				
 			} 
--- P1-Sputify/src/Server/KlientTest.java
+++ P1-Sputify/src/Server/KlientTest.java
@@ -1,6 +1,7 @@
 package Server;
 
 import java.net.*;
+import java.util.*;
 
 import javax.sound.sampled.*;
 
@@ -9,50 +10,81 @@ import java.io.*;
 public class KlientTest {
 	private String serverIP;
 	private int serverPort;
-	
+	BufferedReader bReader;
+	Clip clip1;
 
 	public KlientTest(String serverIP, int serverPort) {
-		playSound();
 		this.serverIP = serverIP;
 		this.serverPort = serverPort;
 		Thread thread = new Thread(new ConnectAndListenToServer());
 		thread.start();
 	}
-	 public void playSound() {
+	
+	public void playSound(byte[] song) {
 	     try {
-	         AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("wavfiles/finishhim.wav").getAbsoluteFile());
-	         Clip clip = AudioSystem.getClip();
-	         clip.open(audioInputStream);
-	         clip.start();
-	     } catch(Exception ex) {
+	    	 InputStream listen = new ByteArrayInputStream(song);
+	         AudioInputStream ais = AudioSystem.getAudioInputStream(listen);
+	         
+	         DataLine.Info dli = new DataLine.Info(Clip.class, ais.getFormat());
+	         clip1 = (Clip) AudioSystem.getLine(dli);
+	         clip1.open(ais);
+	         clip1.start();
+	         
+	     } 
+	     catch(Exception ex) {
 	         System.out.println("Error with playing sound.");
 	         ex.printStackTrace();
-	     }}
+	     }
+	}
+	
+	
 	private class ConnectAndListenToServer implements Runnable {
 		public void run() {
+			
+			StringBuilder sb = new StringBuilder("");
+			
 			try {
-				Socket socket = new Socket(InetAddress.getByName(serverIP),
-						serverPort);
-				ObjectInputStream input = new ObjectInputStream(
+				Socket socket = new Socket(InetAddress.getByName(serverIP), serverPort);
+				ObjectInputStream ois = new ObjectInputStream(
 						socket.getInputStream());
-		
-			byte[] sound;
-				while (true) {
-					
-					    try {
-					    	sound=(byte[] ) input.readObject();
-					    	InputStream myInputStream = new ByteArrayInputStream(sound);
-					        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(myInputStream);
-					        Clip clip = AudioSystem.getClip();
-					        clip.open(audioInputStream);
-					        clip.start();
-					    } catch(Exception ex) {
-					        System.out.println("Error with playing sound.");
-					        ex.printStackTrace();
-					    
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream()); 
+				System.out.println("Object output stream created");
+				
+				oos.writeObject(new String[] {"User1","Pwd1"});
+				oos.flush();
+				System.out.println("Username & pass sent to server");
+
+				Hashtable<Integer, Track> htClient;
+				
+					htClient = (Hashtable<Integer, Track>) ois.readObject();
+					System.out.println("Song list received from server");
+				
+					for (Integer key : htClient.keySet()) {
+					   System.out.println(htClient.get(key));
 					}
+				
+				
+				oos.writeObject(new Integer(1));
+				oos.flush();
+				System.out.println("Track ID sent to server");
+				Thread.sleep(3000);
+				
+				InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+				bReader = new BufferedReader(isr);
+		
+				String song;
+				while ((song = bReader.readLine()) != null) {
+					
+					sb.append(song);
 				}
-			} catch (IOException e) {
+				
+				String strConvertToBytes = sb.toString();
+				
+				byte[] sound = strConvertToBytes.getBytes();
+				
+				playSound(sound);
+				
+			} catch (Exception e) {
 				System.out.println(e);
 					
 			} 
