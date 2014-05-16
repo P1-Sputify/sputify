77e500aebdc47831d2d331e2df37ed9738588117
Nethakaaru <sebastian.aspegren@telia.com>
2014-05-16 20:36:59 +0200

    Added comments to describe what happens where.

--- P1-Sputify/src/server/AdminGUI.java
+++ P1-Sputify/src/server/AdminGUI.java
@@ -15,6 +15,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
+ * A class that functions as a simple gui to see if anything goes wrong.
  * @author Nethakaaru
  * 
  */
@@ -22,10 +23,12 @@ public class AdminGUI extends JFrame implements ActionListener {
 
 	private Controller controller;
 	private JTextArea txtArea = new JTextArea();
-	private JTextField IPandPort = new JTextField("Server created on IP: " + getIP() + " and port: 57005");
-	private JButton toggleServerbtn= new JButton("Turn on server");
-	//A boolean that is false if the server is off and true if the server is on.
-	private boolean isServerOn=false;
+	private JTextField IPandPort = new JTextField("Server created on IP: "
+			+ getIP() + " and port: 57005");
+	private JButton toggleServerbtn = new JButton("Turn on server");
+	// A boolean that is false if the server is off and true if the server is
+	// on.
+	private boolean isServerOn = false;
 
 	public AdminGUI(Controller controller) {
 		this.controller = controller;
@@ -40,15 +43,12 @@ public class AdminGUI extends JFrame implements ActionListener {
 
 		add(txtArea, BorderLayout.CENTER);
 		add(IPandPort, BorderLayout.NORTH);
-		add(toggleServerbtn,BorderLayout.WEST);
+		add(toggleServerbtn, BorderLayout.WEST);
 		IPandPort.setEditable(false);
 		txtArea.setEditable(false);
 	}
 
-	public static void main(String[] args) {
-		AdminGUI gui = new AdminGUI(null);
-
-	}
+	
 
 	/**
 	 * A method used as a console with information.
@@ -62,33 +62,48 @@ public class AdminGUI extends JFrame implements ActionListener {
 		txtArea.setEditable(false);
 	}
 
+	/**
+	 * A method that edits the ip and port text field.
+	 * 
+	 * @param inStr
+	 *            what the ip and port text field should say.
+	 */
 	public void setIPandPort(String inStr) {
 		IPandPort.setEditable(true);
 		IPandPort.setText(inStr);
 		IPandPort.setEditable(false);
 	}
-	public String getIP()  {
+
+	/**
+	 * A method that returns the ip of the server.
+	 * 
+	 * @return the servers ip address.
+	 */
+	public String getIP() {
 		InetAddress host;
 		try {
 			host = InetAddress.getLocalHost();
 			return host.getHostAddress();
 		} catch (UnknownHostException e) {
-		
+
 			addToTextArea(e.getMessage());
 		}
 		return "UNKNOWN";
-		
-	}
-	
 
-	@Override
+	}
+
 	public void actionPerformed(ActionEvent e) {
-//		if(e.getSource()==toggleServerbtn){
-//			if(isServerOn)
-//				controller.turnoffserver();
-//			else 
-//				controller.turnonserver();
-//		}
-		
+		 if(e.getSource()==toggleServerbtn){
+		 if(isServerOn){
+		// controller.turnoffserver();
+		 } else{
+		// controller.turnonserver();
+		 	}
+		 }
+
+	}
+	public static void main(String[] args) {
+		AdminGUI gui = new AdminGUI(null);
+
 	}
 }
